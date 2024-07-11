76abcf6314e9c3bcaf84e2754cf9c1f9112a6d55
Matthew Vincent <matt@averails.com>
2024-07-11 11:49:53 +0300

    updating entrypoint to handle more directories

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+# Ensure necessary directories exist and set permissions
+mkdir -p /var/spool/rsyslog
+mkdir -p /var/spool/postfix/dev
+mkdir -p /var/log
+
+touch /var/log/mail.log
+touch /var/log/syslog
+
+chmod 755 /var/spool/rsyslog
+chmod 755 /var/spool/postfix
+chmod 644 /var/log/mail.log
+chmod 644 /var/log/syslog
+
+# Create /dev/log for rsyslog
+if [ ! -e /var/spool/postfix/dev/log ]; then
+  ln -sf /dev/log /var/spool/postfix/dev/log
+fi
+
 # Start rsyslog
 rsyslogd
-
-# Ensure the mail log file exists and set permissions
-touch /var/log/mail.log
-chmod 644 /var/log/mail.log
-
-# Ensure the syslog file exists and set permissions
-touch /var/log/syslog
-chmod 644 /var/log/syslog
 
 # Postmap the configuration files
 postmap /etc/postfix/sasl_passwd
