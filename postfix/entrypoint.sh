1450acb53531c29f3192fe6e7594edd1dba54e92
Matthew Vincent <matt@averails.com>
2024-07-11 11:42:39 +0300

    more tweaks to the entrypoint script

--- postfix/entrypoint.sh
+++ postfix/entrypoint.sh
@@ -3,9 +3,13 @@
 # Start rsyslog
 rsyslogd
 
-# Ensure the mail log file exists
+# Ensure the mail log file exists and set permissions
 touch /var/log/mail.log
-chmod 666 /var/log/mail.log
+chmod 644 /var/log/mail.log
+
+# Ensure the syslog file exists and set permissions
+touch /var/log/syslog
+chmod 644 /var/log/syslog
 
 # Postmap the configuration files
 postmap /etc/postfix/sasl_passwd
@@ -13,3 +17,6 @@ postmap /etc/postfix/sender_canonical
 
 # Start postfix in the foreground
 postfix start-fg
+
+# Tail the mail log to keep the container running
+tail -f /var/log/mail.log
